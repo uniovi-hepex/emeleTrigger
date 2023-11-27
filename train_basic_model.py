2eebd95d0583d86d028179792fcb7ad13e0ed335
Pietro Vischia <pietro.vischia@gmail.com>
2023-11-27 18:52:36 +0100

    Starting to look into feeding data to model

--- train_basic_model.py
+++ train_basic_model.py
@@ -1,6 +1,138 @@
 from dataviz import get_test_data
 import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+#import torch.nn.functional as F
+#from sklearn.model_selection import train_test_split
+#import matplotlib.pyplot as plt
+#import matplotlib
+
 
 branches = get_test_data()
 
 print(branches.head())
+
+# What comes below mostly still doesn't make any sense---we don't want to do supervised learning here.
+
+
+# Maybe not needed (or move to get_test_data)
+class OMTFDataset(Dataset):
+    def __init__(self, X, y):
+        self.X = torch.Tensor(X)
+        self.y = torch.Tensor(y)
+    def __len__(self):
+        return len(self.y)
+    def __getitem__(self, idx):
+        label = self.y[idx]
+        datum = self.X[idx]
+        return datum, label
+batch_size=512
+
+
+X_train, X_test, y_train, y_test = train_test_split(branches, branches, test_size=0.33)"
+
+train_dataset = OMTFDataset(X_train, y_train)
+test_dataset = OMTFDataset(X_test, y_test)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+
+# A simple neural network, to start with
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(14, 512),
+            nn.ReLU(),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128,64),
+            nn.ReLU(),
+            nn.Linear(64,8),
+            nn.ReLU(),
+            nn.Linear(8, 1)
+        )
+    def forward(self, x):
+        # Pass data through conv1
+        x = self.linear_relu_stack(x)
+        return x
+
+def train_loop(dataloader, model, loss_fn, optimizer, scheduler):
+    size = len(dataloader.dataset)
+    losses=[]
+    # Set the model to training mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        optimizer.zero_grad()
+        # Compute prediction and loss
+        pred = model(X[:,:-3]) # Change this
+        #if (all_equal3(pred.detach().numpy())):
+        #    print(\"All equal!\")
+        loss = loss_fn(pred, y)
+        losses.append(loss.item())
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+
+        if batch % 1000 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    scheduler.step()
+    return np.mean(losses)
+
+def test_loop(dataloader, model, loss_fn):
+    # Set the model to evaluation mode - important for batch normalization and dropout layers
+    # Unnecessary in this situation but added for best practices
+    losses=[]
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
+    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X[:,:-3]) # Change this
+            loss = loss_fn(pred, y).item()
+            losses.append(loss)
+            test_loss += loss
+            #correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    return np.mean(losses)
+    #test_loss /= num_batches
+    #correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
+
+epochs = 30
+learningRate = 0.01
+
+model = NeuralNetwork()
+
+loss_fn = torch.nn.MSELoss()
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+
+
+train_losses=[]
+test_losses=[]
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train_loss=train_loop(train_dataloader, model, loss_fn, optimizer, scheduler)
+    test_loss=test_loop(test_dataloader, model, loss_fn)
+    train_losses.append(train_loss)
+    test_losses.append(test_loss)
+    print("Avg train loss", train_loss, ", Avg test loss", test_loss, "Current learning rate", scheduler.get_last_lr())
+print("Done!")
+
+
+plt.plot(train_losses, label="Average training loss")
+plt.plot(test_losses, label="Average test loss")
+plt.legend(loc="best")
