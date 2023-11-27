a87fb920ce4fbe25aa6cce92b0f8ac55306a176a
Pietro Vischia <pietro.vischia@gmail.com>
2023-11-27 18:35:52 +0100

    Restructure

--- dataviz.py
+++ dataviz.py
@@ -1,13 +1,12 @@
 import os
 import uproot
 
-if not os.path.isfile('data/omtfAnalysis2.root'):
+def get_test_data():
+    if not os.path.isfile('data/omtfAnalysis2.root'):
         os.system('wget http://www.hep.uniovi.es/vischia/omtfsweetlove/omtfAnalysis2.root -P data/')
         print('File downloaded into data/')
-else:
+    else:
         print('File already exists in data/')
 
-
-branches = uproot.open('data/omtfAnalysis2.root:simOmtfPhase2Digis/OMTFHitsTree').arrays(library='pd')
-
-print(branches.head())
+    branches = uproot.open('data/omtfAnalysis2.root:simOmtfPhase2Digis/OMTFHitsTree').arrays(library='pd')
+    return branches
---
+++ train_basic_model.py
@@ -0,0 +1,6 @@
+from dataviz import get_test_data
+import torch
+
+branches = get_test_data()
+
+print(branches.head())
