c0b40ab6c60bab71400b37710c8376ff799ce002
Santiago Folgueras <santiago.folgueras@cern.ch>
2024-05-31 16:50:31 +0200

    Incorporate all input variables to the plotter.

--- tools/plotter/datasets.py
+++ tools/plotter/datasets.py
@@ -5,14 +5,14 @@ files_SingleMu_FlatPt0To1000Dxy3m = (phedexPath + "data/SingleMu_GT131X_Extrapol
 files_SingleMu_XTo2LLP4Mu_Ctau5m  = (phedexPath + "data/SingleMu_GT131X_Extrapolation_GhostBusterTest_XTo2LLP4Mu_Ctau5m_Stub.root", "simOmtfDigis/OMTFHitsTree") # Muo
 files_SingleMu_XTo2LLP4Mu_Ctau3m  = (phedexPath + "data/SingleMu_GT131X_Extrapolation_GhostBusterTest_XTo2LLP4Mu_Stub.root","simOmtfDigis/OMTFHitsTree")
 files_SingleMu_PromptSampleFlatPt = (phedexPath + "data/SingleMu_GT131X_Extrapolation_GhostBusterTest_PromptSampleFlatPt_Stub.root","simOmtfDigis/OMTFHitsTree")
-
+files_test = (phedexPath + "data/TestEvents__t22__Patterns_ExtraplMB1nadMB2DTQualAndEtaFixedP_ValueP1Scale_t20_v1_SingleMu_iPt_and_OneOverPt_classProb17_recalib2_minDP0.root", "simOmtfPhase2Digis/OMTFHitsTree")
 datasets = {
     'MuGun_FlatPt0To1000Dxy3m'  : {'name'       : 'MuGun_FlatPt0To1000Dxy3m', # Name of the dataset
                                    'samples'    : files_SingleMu_FlatPt0To1000Dxy3m[0], #  FILE
                                    'treename'   : files_SingleMu_FlatPt0To1000Dxy3m[1], # Is prompt?
                                    'color'      : 'black', # Plotting color
                                    'label'      : "Muon Gun ($d_{xy}=3m$)", # Legend label
-                                   'selection'  : {'acceptance' : 'muonPropEta!=0 & muonPropPhi!=0',  #Only take muons inside acceptance
+                                   'selection'  : {'acceptance' : '(muonPropEta!=0) & (muonPropPhi!=0)',  #Only take muons inside acceptance
                                  } 
                                 },   
     'MuGun_XTo2LLP4Mu_Ctau5m'  : {'name'       : 'MuGun_XTo2LLP4Mu_Ctau5m', # Name of the dataset
@@ -20,23 +20,31 @@ datasets = {
                                   'treename'  : files_SingleMu_XTo2LLP4Mu_Ctau5m[1], # Is prompt?
                                   'color'     : 'black', # Plotting color
                                   'label'     : "X to $4\mu (ctau=5m)$", # Legend label
-                                  'selection'  : {'acceptance' : 'muonPropEta!=0 & muonPropPhi!=0',  #Only take muons inside acceptance
+                                  'selection'  : {'acceptance' : '(muonPropEta!=0) & (muonPropPhi!=0)',  #Only take muons inside acceptance
                                  } 
                                 },
     'MuGun_XTo2LLP4Mu_Ctau3m'  : {'name'       : 'MuGun_XTo2LLP4Mu_Ctau3m', # Name of the dataset
                                   'samples'    : files_SingleMu_XTo2LLP4Mu_Ctau3m[0], #  FILE
-                                   'treename'  : files_SingleMu_XTo2LLP4Mu_Ctau3m[1], # Is prompt?
-                                   'color'     : 'black', # Plotting color
-                                   'label'     :  "X to $4\mu (ctau=3m)$", # Legend label
-                                   'selection'  : {'acceptance' : 'muonPropEta!=0 & muonPropPhi!=0',  #Only take muons inside acceptance
+                                  'treename'  : files_SingleMu_XTo2LLP4Mu_Ctau3m[1], # Is prompt?
+                                  'color'     : 'black', # Plotting color
+                                  'label'     :  "X to $4\mu (ctau=3m)$", # Legend label
+                                  'selection'  : {'acceptance' : '(muonPropEta!=0) & (muonPropPhi!=0)',  #Only take muons inside acceptance
                                  } 
                                 },
     'MuGun_PromptSampleFlatPt' : {'name'       : 'MuGun_PromptSampleFlatPt', # Name of the dataset
                                   'samples'    : files_SingleMu_PromptSampleFlatPt[0], #  FILE
-                                   'treename'    : files_SingleMu_PromptSampleFlatPt[1], # Is prompt?
+                                  'treename'    : files_SingleMu_PromptSampleFlatPt[1], # Is prompt?
+                                  'color'     : 'black', # Plotting color
+                                  'label'     : "Muon Gun (Prompt)", # Legend label
+                                  'selection'  : {'acceptance' : '(muonPropEta!=0) & (muonPropPhi!=0)',  #Only take muons inside acceptance
+                                 } 
+                                },
+    'test'                      : {'name'       : 'test', # Name of the dataset
+                                  'samples'    : files_test[0], #  FILE
+                                   'treename'    : files_test[1], # Is prompt?
                                    'color'     : 'black', # Plotting color
-                                   'label'     : "Muon Gun (Prompt)", # Legend label
-                                   'selection'  : {'acceptance' : 'muonPropEta!=0 & muonPropPhi!=0',  #Only take muons inside acceptance
+                                   'label'     : "test", # Legend label
+                                   'selection'  : {'acceptance' : '(muonPropEta!=0) & (muonPropPhi!=0)',  #Only take muons inside acceptance
                                  } 
                                 },
 }
