1b5946598d6088f00e3835a5ae51c8d31820b612
cramonal <clara.ramon.alvarez@cern.ch>
2024-07-29 14:47:27 +0200

    testing

--- tools/training/train_basic_model.py
+++ tools/training/train_basic_model.py
@@ -23,8 +23,9 @@ g=OMTFGraphNetwork() # Optionally pass a class name. The class must inherit from
 #g.load_data('data/omtfAnalysis2.root:simOmtfPhase2Digis/OMTFHitsTree', fraction=0.05, viz=True)
 #g.load_data('data/Displaced_cTau5m_XTo2LLTo4Mu_condPhase2_realistic_l1omtf_12.root:simOmtfPhase2Digis/OMTFHitsTree', fraction=1., viz=True)
 #g.load_data('data/SingleMu_GT131X_Extrapolation_GhostBusterTest_FlatPt0To1000Dxy3m_NonDegraded_Stub_v2.root:simOmtfDigis/OMTFHitsTree', fraction=1., viz=True)
-g.load_data('data/SingleMu_GT131X_Extrapolation_GhostBusterTest_XTo2LLP4Mu_Ctau5m_Stub.root:simOmtfDigis/OMTFHitsTree', fraction=0.01, viz=True)
-
+#g.load_data('data/SingleMu_GT131X_Extrapolation_GhostBusterTest_XTo2LLP4Mu_Ctau5m_Stub.root:simOmtfDigis/OMTFHitsTree', fraction=0.01, viz=True)
+g.load_data('data/Dumper_l1omtf_129.root:simOmtfPhase2Digis/OMTFHitsTree', fraction=1., viz=True)
+print("data loaded")
         
 # Train the model or load a pretrained model
 dotrain=True
