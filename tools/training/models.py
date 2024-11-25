cb2e9aba7be9f4a3ecb97ede7c28ef5035cb1b6d
Santiago Folgueras <santiago.folgueras@cern.ch>
2024-11-25 07:59:49 +0100

    send new stuff

--- tools/training/models.py
+++ tools/training/models.py
@@ -80,3 +80,21 @@ class GATRegressorDO(torch.nn.Module):
         x = torch.cat([global_max_pool(x, batch), global_mean_pool(x, batch)], dim=1)
         x = self.fc1(x)
         return x
+
+class SAGE(torch.nn.Module):
+    def __init__(self, num_node_features, hidden_dim, output_dim=1):
+        super(SAGE, self).__init__()
+        self.conv1 = SAGEConv(num_node_features, hidden_dim)
+        self.conv2 = SAGEConv(hidden_dim, hidden_dim)
+        self.fc1 = torch.nn.Linear(hidden_dim*2, output_dim)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x.float(), data.edge_index, data.batch
+        x = F.relu(x)
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = torch.cat([global_max_pool(x, batch), global_mean_pool(x, batch)], dim=1)
+        x = self.fc1(x)
+        return x
