4e95a30b5190a4430cfcbc99a1b3c9b525afdd6c
Santiago Folgueras <santiago.folgueras@cern.ch>
2024-11-29 17:14:08 +0100

    Clean-up of the repository

--- tools/example_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_106_cuda/x86_64-el9-gcc11-opt/setup.sh
-
-#python3 -m virtualenv myvenv
-#source myvenv/bin/activate
-#pip install .
-
-python tools/training/TrainModelFromGraph.py --train
