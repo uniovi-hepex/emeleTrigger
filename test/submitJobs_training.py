1ef8ba41c35d504e72aa1ffa42ebf110f577a052
Santiago Folgueras <santiago.folgueras@cern.ch>
2024-12-03 17:30:42 +0100

    adapt the submitJobs_training.py to test different connections

--- test/submitJobs_training.py
+++ test/submitJobs_training.py
@@ -4,12 +4,13 @@ import os,sys
 print('START\n')
 ########   YOU ONLY NEED TO FILL THE AREA BELOW   #########
 ########   customization  area #########
-InputFolder = "/eos/cms/store/user/folguera/L1TMuon/INTREPID/Graphs_v240725_241106/3neighbours_muonQOverPt/" # list with all the file directories
+InputFolder = "/eos/cms/store/user/folguera/L1TMuon/INTREPID/Graphs_v240725_241106/" # list with all the file directories
 queue = "workday" # give bsub queue -- 8nm (8 minutes), 1nh (1 hour), 8nh, 1nd (1day), 2nd, 1nw (1 week), 2nw
 OutputDir = "/eos/cms/store/user/folguera/L1TMuon/INTREPID/Model_v240725_Bsize64_lr5e-4_NOnormNodes_GAT_241106/"
 WORKDIR = "/afs/cern.ch/user/f/folguera/workdir/INTREPID/tmp/TrainingModel/"
 ModelTypes = ['SAGE', 'MPNN']
 NormalizationTypes = ['DropLastTwoNodeFeatures', 'NodesAndEdgesAndOnlySpatial']
+InputGraphs = ["3neighbours_muonQOverPt/", "all_connections_muonQOverPt/"]
 ########   customization end   #########
 
 path = os.getcwd()
@@ -32,24 +33,32 @@ print("InputFolder: %s" %(InputFolder))
 print("OutputDir: %s" %(OutputDir))
 
 ##### creating job #####
-file_count = 1
+file_count = 0
 for model in ModelTypes:
     for normalization in NormalizationTypes: 
-        SaveTag = model + "_" + normalization + "_Bsize1024_lr1e-3_241203"
-        with open('%s/exec/job_train_model_%02d.sh' %(WORKDIR, file_count), 'w') as fout:
-            fout.write("#!/bin/sh\n")
-            fout.write("echo\n")
-            fout.write("echo\n")
-            fout.write("echo 'START---------------'\n")
-            fout.write("echo 'WORKDIR ' ${PWD}\n")
-            fout.write("cd "+str(path)+"\n")
-            fout.write("source pyenv/bin/activate\n")
-            fout.write("echo 'Saving Model in  %s' \n" %(OutputDir))
-            fout.write("python tools/training/TrainModelFromGraph.py --model_type %s --hidden_dim 32 --normalization %s --graph_path %s --out_path %s --do_train --save_tag %s --batch_size 1024 --learning_rate 0.001\n" %(model, normalization, InputFolder, OutputDir, SaveTag))  
-            fout.write("echo 'STOP---------------'\n")
-            fout.write("echo\n")
-            fout.write("echo\n")
-        os.system("chmod 755 %s/exec/job_train_model_%02d.sh" %(WORKDIR, file_count))
+        for input_graph in InputGraphs:
+            file_count += 1
+            print("Creating job for model %s with normalization %s and input graphs %s" %(model, normalization, input_graph))
+            SaveTag = model + "_" + normalization + "_Bsize64_lr5e-4_241106_20files_"
+            if "all" in input_graph:
+                SaveTag = SaveTag + "allConnections"
+            else:
+                SaveTag = SaveTag + "3neighbours"
+    
+            with open('%s/exec/job_train_model_%02d.sh' %(WORKDIR, file_count), 'w') as fout:
+                fout.write("#!/bin/sh\n")
+                fout.write("echo\n")
+                fout.write("echo\n")
+                fout.write("echo 'START---------------'\n")
+                fout.write("echo 'WORKDIR ' ${PWD}\n")
+                fout.write("cd "+str(path)+"\n")
+                fout.write("source pyenv/bin/activate\n")
+                fout.write("echo 'Saving Model in  %s' \n" %(OutputDir))
+                fout.write("python tools/training/TrainModelFromGraph.py --model_type %s --hidden_dim 32 --normalization %s --graph_path %s --out_path %s --do_train --save_tag %s --batch_size 1024 --learning_rate 0.001 --num_files 20\n" %(model, normalization, InputFolder, OutputDir, SaveTag))  
+                fout.write("echo 'STOP---------------'\n")
+                fout.write("echo\n")
+                fout.write("echo\n")
+            os.system("chmod 755 %s/exec/job_train_model_%02d.sh" %(WORKDIR, file_count))
 
 ###### create submit.sub file ####
 with open('submit.sub', 'w') as fout:
